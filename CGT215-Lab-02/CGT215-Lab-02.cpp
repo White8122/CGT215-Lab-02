590e7096a2e2c540dc3e7757cadb361ac3acaf2d
El White <white812@purdue.edu>
2023-08-30 12:27:29 -0400

    finished lab

--- CGT215-Lab-02/CGT215-Lab-02.cpp
+++ CGT215-Lab-02/CGT215-Lab-02.cpp
@@ -2,10 +2,31 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+	cout << "Hey, my name is El and we are going to solve an equation!" << endl;
+	cout << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "Solving for 'x'" << endl;
+	cout << endl;
+
+	cout << "Please enter a value for A:";
+	cin >> A;
+	
+
+	cout << "Please enter a value for B:";
+	cin >> B;
+	cout << endl;
+
+	cout << "Solving for " << A << "x + " << B << "= 0 for x..." << endl;
+	cout << endl;
+
+	X = (-B/A);
+	cout << "The answer is: "<<endl << X << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
